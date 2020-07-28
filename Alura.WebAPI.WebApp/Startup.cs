10c7b1b48f394eef64fed91dd07c7fe2b6756436
Marlon Challouts <marlon.challouts@gmail.com>
2020-07-28 14:41:26 -0300

    Implementando método de autenticação por JWT Implementando controller de login para fornecer o token

--- Alura.WebAPI.WebApp/Startup.cs
+++ Alura.WebAPI.WebApp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Alura.WebAPI.WebApp.Formatters;
+using Microsoft.IdentityModel.Tokens;
+using System;
 
 namespace Alura.ListaLeitura.WebApp
 {
@@ -44,10 +46,28 @@ namespace Alura.ListaLeitura.WebApp
 
             services.AddTransient<IRepository<Livro>, RepositorioBaseEF<Livro>>();
 
-            services.AddMvc(option =>
+            services.AddMvc(options =>
             {
-                option.OutputFormatters.Add(new LivroCsvFormatter());
+                options.OutputFormatters.Add(new LivroCsvFormatter());
             }).AddXmlSerializerFormatters();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            }).AddJwtBearer("JwtBearer", options => {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("alura-webapi-authentication-valid")),
+                    ClockSkew = TimeSpan.FromMinutes(5),
+                    ValidIssuer = "Alura.WebApp",
+                    ValidAudience = "Postman"
+                };
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
