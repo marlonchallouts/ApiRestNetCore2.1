fa362eeeaeb66591f8b96812bd5d7557ac41aabc
Marlon Challouts <marlon.challouts@gmail.com>
2020-07-28 10:59:08 -0300

    Alterado tipo de retorno Criado metodo para retornar a capa do livro Criado controller para retornar os dados da todas ou de uma especifica lista de leitura

--- Alura.WebAPI.WebApp/Api/LivrosController.cs
+++ Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 namespace Alura.WebAPI.WebApp.Api
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class LivrosController : ControllerBase
     {
         private readonly IRepository<Livro> _repo;
@@ -19,7 +19,7 @@ namespace Alura.WebAPI.WebApp.Api
         [HttpGet]
         public IActionResult ListaDeLivros()
         {
-            var lista = _repo.All.Select(l => l.ToModel()).ToList();
+            var lista = _repo.All.Select(l => l.ToApi()).ToList();
             return Ok(lista);
         }
 
@@ -31,7 +31,21 @@ namespace Alura.WebAPI.WebApp.Api
             if (model == null)
                 return NotFound();
 
-            return Ok(model.ToModel());
+            return Ok(model.ToApi());
+        }
+
+        [HttpGet("{id}/capa")]
+        public IActionResult ImagemCapa(int id)
+        {
+            byte[] img = _repo.All
+                .Where(l => l.Id == id)
+                .Select(l => l.ImagemCapa)
+                .FirstOrDefault();
+            if (img != null)
+            {
+                return File(img, "image/png");
+            }
+            return File("~/images/capas/capa-vazia.png", "image/png");
         }
 
         [HttpPost]
